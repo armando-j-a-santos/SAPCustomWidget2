6de9d9da345b8990aa0e2a2cac97385bc8b6ab7b
Armando Santos <47860995+armando-j-a-santos@users.noreply.github.com>
2022-08-02 10:47:41 +0100

    Update Milton10.js

--- Milton10.js
+++ Milton10.js
@@ -82,7 +82,7 @@ div.testColumn4{
     </style>
     `;
 
-    customElements.define('com-demo-miltonfirstcw10', class DiAutoComplete extends HTMLElement {
+    customElements.define('com-demo-miltonfirstcw10', class testTable extends HTMLElement {
 
 
         constructor() {
